61ab4a85e815944c7a17adbb52de33bc484da2ed
mohamedahemd99 <58490629+mohamedahemd99@users.noreply.github.com>
2021-06-08 00:00:24 +0200

    new password

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+import 'package:flutter/cupertino.dart';
+void main(){
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    )
+  );
+}
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:LoginScreen() ,
+    );
+  }
+}
